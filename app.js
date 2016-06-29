411d60220cc9052661741ca03f10b4580d68f40a
YC <yamile.colque@gmail.com>
2016-06-28 20:19:36 -0700

    images displayed randomly

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var itemsArray = [];
+var pictureObjectsArray = [];
 
 //Constructor for the image objects
 function PictureObject (name, filepath) {
@@ -6,9 +6,7 @@ function PictureObject (name, filepath) {
   this.filePath = filepath;
   this.totalNumberOfTimesDisplayed = 0;
   this.totalNumberOfTimesClicked = 0;
-
-
-  itemsArray.push(this.name);
+  pictureObjectsArray.push(this);
 };
 
 //Methods
@@ -17,16 +15,67 @@ function PictureObject (name, filepath) {
 var bagObject = new PictureObject ('bagpicture', 'img/bag.jpg');
 var bananaObject = new PictureObject ('bananapicture', 'img/banana.jpg');
 var bathroomObject = new PictureObject ('bathroom','img/bathroom.jpg');
-var bootsObject = new PictureObject ('boots', 'img/boots.jpg');
+var bootsObject = new PictureObject ('boots','img/boots.jpg');
 var breakfastObject = new PictureObject ('breakfast', 'img/breakfast.jpg');
+var bubblegumObject = new PictureObject ('bubblegum', 'img/bubblegum.jpg');
+var chairObject = new PictureObject ('chair', 'img/chair.jpg');
+var cthulhuObject = new PictureObject ('cthulhu', 'img/cthulhu.jpg');
+var dogduck = new PictureObject ('dog-duck', 'img/dog-duck.jpg');
+var dragon = new PictureObject ('dragon', 'img/dragon.jpg');
+var pen = new PictureObject ('pen','img/pen.jpg');
+var petsweep = new PictureObject ('pet-sweep', 'img/pet-sweep.jpg');
+var scissors = new PictureObject ('scissors', 'img/scissors.jpg');
+var shark = new PictureObject ('shark', 'img/shark.jpg');
+var sweep = new PictureObject ('sweep', 'img/sweep.png');
+var tauntuan = new PictureObject ('tauntaun', 'img/tauntaun.jpg');
+var unicorn = new PictureObject ('unicorn', 'img/unicorn.jpg');
+var usb = new PictureObject ('usb', 'img/usb.jpg');
+var watercan = new PictureObject ('water-can', 'img/water-can.jpg');
+var waerglass = new PictureObject ('wine-glass','img/wine-glass');
 
 
-//Random Number for Images
-function randomNumber () {
-  return Math.floor(Math.random() * itemsArray.length)
+
+//create index with randomized numbers
+function randomIndex (){
+  var randomImage1 = Math.floor(Math.random() * pictureObjectsArray.length);
+  var randomImage2 = Math.floor(Math.random() * pictureObjectsArray.length);
+  var randomImage3 = Math.floor(Math.random() * pictureObjectsArray.length);
+
+  while (randomImage1 === randomImage2){
+    randomImage2 = Math.floor(Math.random() * pictureObjectsArray.length);
+  }
+
+  while (randomImage3 === randomImage2 || randomImage3 === randomImage1){
+    randomImage3 = Math.floor(Math.random() * pictureObjectsArray.length);
+  }
+
+  return [randomImage1, randomImage2, randomImage3];
+};
+
+//display images
+function randomObjects() {
+  var index = randomIndex(pictureObjectsArray);
+  var image1 = pictureObjectsArray[index[0]];
+  var image2 = pictureObjectsArray[index[1]];
+  var image3 = pictureObjectsArray[index[2]];
+  return [image1, image2, image3];
 }
 
+function displayPictures() {
+  var objectValues = randomObjects();
+  var imagedisplay1 = document.getElementById('left');
+  imagedisplay1.src = objectValues[0].filePath;
+  var imagedisplay2 = document.getElementById('middle');
+  imagedisplay2.src = objectValues[1].filePath;
+  var imagedisplay3 = document.getElementById('right');
+  imagedisplay3.src = objectValues[2].filePath;
 
+
+
+image1.addEventListener('click', handleButtonClick);
+}
+
+displayPictures();
 
 
 //Clicks
@@ -34,6 +83,3 @@ function handleButtonClick (event) {
   event.target;
   console.log('worked');
 }
-
-var image1 = document.getElementById('left');
-image1.addEventListener('click', handleButtonClick);
